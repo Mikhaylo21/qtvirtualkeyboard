93b1327ecf6e3fdc184fa18af9d7fe4d8204b56f
Mikhaylo Tkachuk <mikhaylo.tkachuk@censolutions.es>
2022-03-23 10:17:45 +0100

    Added sourcedir and buildir to the script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ip=192.168.2.67
-installdir=/home/user/cpp/qtvirtualkeyboard/arm_build/install/
+builddir=/home/user/cpp/qtvirtualkeyboard/arm_build
+installdir=$builddir/install
+sourcedir=/home/user/cpp/qtvirtualkeyboard
 
 #clean install dir
 rm -r -f $installdir
@@ -8,23 +10,24 @@ rm -r -f $installdir
 mkdir -p $installdir
 
 mkdir -p $installdir/plugins/platforminputcontexts/ 2> /dev/null
-install -m 755 -p ./plugins/platforminputcontexts/libqtvirtualkeyboardplugin.so $installdir/plugins/platforminputcontexts/libqtvirtualkeyboardplugin.so
+install -m 755 -p $builddir/plugins/platforminputcontexts/libqtvirtualkeyboardplugin.so $installdir/plugins/platforminputcontexts/libqtvirtualkeyboardplugin.so
 
 mkdir -p $installdir/lib/cmake/Qt5Gui/ 2> /dev/null
-install -m 644 -p /home/user/cpp/qtvirtualkeyboard/arm_build/lib/cmake/Qt5Gui/Qt5Gui_QVirtualKeyboardPlugin.cmake $installdir/lib/cmake/Qt5Gui/Qt5Gui_QVirtualKeyboardPlugin.cmake
+install -m 644 -p $builddir/lib/cmake/Qt5Gui/Qt5Gui_QVirtualKeyboardPlugin.cmake $installdir/lib/cmake/Qt5Gui/Qt5Gui_QVirtualKeyboardPlugin.cmake
 
 mkdir -p $installdir/import/ 2> /dev/null
-install -m 644 -p /home/user/cpp/qtvirtualkeyboard/src/virtualkeyboard/import/qmldir $installdir/import/qmldir
-install -m 644 -p /home/user/cpp/qtvirtualkeyboard/src/virtualkeyboard/import/plugins.qmltypes $installdir/import/plugins.qmltypes
+install -m 644 -p $sourcedir/src/virtualkeyboard/import/qmldir $installdir/import/qmldir
+install -m 644 -p $sourcedir/src/virtualkeyboard/import/plugins.qmltypes $installdir/import/plugins.qmltypes
 
 mkdir -p $installdir/qml/QtQuick/VirtualKeyboard/Styles/ 2> /dev/null
-install -m 755 -p ./qml/QtQuick/VirtualKeyboard/Styles/libqtvirtualkeyboardstylesplugin.so $installdir/qml/QtQuick/VirtualKeyboard/Styles/libqtvirtualkeyboardstylesplugin.so
+install -m 755 -p $builddir/qml/QtQuick/VirtualKeyboard/Styles/libqtvirtualkeyboardstylesplugin.so $installdir/qml/QtQuick/VirtualKeyboard/Styles/libqtvirtualkeyboardstylesplugin.so
 
 mkdir -p $installdir/styles/ 2> /dev/null
-install -m 644 -p /home/user/cpp/qtvirtualkeyboard/src/virtualkeyboard/styles/qmldir $installdir/styles/qmldir
-install -m 644 -p /home/user/cpp/qtvirtualkeyboard/src/virtualkeyboard/styles/plugins.qmltypes $installdir/styles/plugins.qmltypes
+install -m 644 -p $sourcedir/src/virtualkeyboard/styles/qmldir $installdir/styles/qmldir
+install -m 644 -p $sourcedir/src/virtualkeyboard/styles/plugins.qmltypes $installdir/styles/plugins.qmltypes
 
 rm install.tar 2> /dev/null
+cd $builddir
 tar -cf install.tar install
 scp install.tar root@$ip:/home/root/install.tar
 ssh root@$ip "tar -xf install.tar"
@@ -38,3 +41,5 @@ ssh root@$ip "mkdir /usr/lib/qt5/qml/QtQuick/VirtualKeyboard/Styles/"
 ssh root@$ip "install -m 755 -p /home/root/install/qml/QtQuick/VirtualKeyboard/Styles/libqtvirtualkeyboardstylesplugin.so /usr/lib/qt5/qml/QtQuick/VirtualKeyboard/Styles/libqtvirtualkeyboardstylesplugin.so"
 ssh root@$ip "install -m 644 -p /home/root/install/styles/qmldir /usr/lib/qt5/qml/QtQuick/VirtualKeyboard/Styles/"
 ssh root@$ip "install -m 644 -p /home/root/install/styles/plugins.qmltypes /usr/lib/qt5/qml/QtQuick/VirtualKeyboard/Styles/"
+
+cd $sourcedir
